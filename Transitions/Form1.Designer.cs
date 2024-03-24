285f744c0aec9e7edb4cd7fd0fb22c068048bff2
merte <merte@MSI>
2024-03-24 07:34:59 +0100

    Ajoutez des fichiers projet.

---
+++ Transitions/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Transitions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBoxTexture1 = new PictureBox();
+            btnSelectTexture1 = new Button();
+            btnSelectTexture2 = new Button();
+            pictureBoxTexture2 = new PictureBox();
+            btnSelectAlpha = new Button();
+            btnGenerateTransition = new Button();
+            pictureBoxAlpha1 = new PictureBox();
+            trackBarContrast = new TrackBar();
+            label1 = new Label();
+            label2 = new Label();
+            pictureBoxPreview = new PictureBox();
+            flowLayoutPanelTextures1 = new FlowLayoutPanel();
+            flowLayoutPanelTextures2 = new FlowLayoutPanel();
+            flowLayoutPanelAlphaImages = new FlowLayoutPanel();
+            pictureBoxLandtile = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxTexture1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxTexture2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxAlpha1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarContrast).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxPreview).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxLandtile).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBoxTexture1
+            // 
+            pictureBoxTexture1.BorderStyle = BorderStyle.FixedSingle;
+            resources.ApplyResources(pictureBoxTexture1, "pictureBoxTexture1");
+            pictureBoxTexture1.Name = "pictureBoxTexture1";
+            pictureBoxTexture1.TabStop = false;
+            // 
+            // btnSelectTexture1
+            // 
+            resources.ApplyResources(btnSelectTexture1, "btnSelectTexture1");
+            btnSelectTexture1.Name = "btnSelectTexture1";
+            btnSelectTexture1.UseVisualStyleBackColor = true;
+            btnSelectTexture1.Click += btnSelectTexture1_Click;
+            // 
+            // btnSelectTexture2
+            // 
+            resources.ApplyResources(btnSelectTexture2, "btnSelectTexture2");
+            btnSelectTexture2.Name = "btnSelectTexture2";
+            btnSelectTexture2.Click += btnSelectTexture2_Click;
+            // 
+            // pictureBoxTexture2
+            // 
+            pictureBoxTexture2.BorderStyle = BorderStyle.FixedSingle;
+            resources.ApplyResources(pictureBoxTexture2, "pictureBoxTexture2");
+            pictureBoxTexture2.Name = "pictureBoxTexture2";
+            pictureBoxTexture2.TabStop = false;
+            // 
+            // btnSelectAlpha
+            // 
+            resources.ApplyResources(btnSelectAlpha, "btnSelectAlpha");
+            btnSelectAlpha.Name = "btnSelectAlpha";
+            btnSelectAlpha.Click += btnSelectAlpha_Click;
+            // 
+            // btnGenerateTransition
+            // 
+            resources.ApplyResources(btnGenerateTransition, "btnGenerateTransition");
+            btnGenerateTransition.Name = "btnGenerateTransition";
+            btnGenerateTransition.Click += btnGenerateTransition_Click;
+            // 
+            // pictureBoxAlpha1
+            // 
+            pictureBoxAlpha1.BorderStyle = BorderStyle.FixedSingle;
+            resources.ApplyResources(pictureBoxAlpha1, "pictureBoxAlpha1");
+            pictureBoxAlpha1.Name = "pictureBoxAlpha1";
+            pictureBoxAlpha1.TabStop = false;
+            // 
+            // trackBarContrast
+            // 
+            trackBarContrast.LargeChange = 10;
+            resources.ApplyResources(trackBarContrast, "trackBarContrast");
+            trackBarContrast.Maximum = 100;
+            trackBarContrast.Name = "trackBarContrast";
+            trackBarContrast.Scroll += trackBarContrast_Scroll;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.Name = "label2";
+            // 
+            // pictureBoxPreview
+            // 
+            pictureBoxPreview.BorderStyle = BorderStyle.FixedSingle;
+            resources.ApplyResources(pictureBoxPreview, "pictureBoxPreview");
+            pictureBoxPreview.Name = "pictureBoxPreview";
+            pictureBoxPreview.TabStop = false;
+            // 
+            // flowLayoutPanelTextures1
+            // 
+            resources.ApplyResources(flowLayoutPanelTextures1, "flowLayoutPanelTextures1");
+            flowLayoutPanelTextures1.BorderStyle = BorderStyle.FixedSingle;
+            flowLayoutPanelTextures1.Name = "flowLayoutPanelTextures1";
+            // 
+            // flowLayoutPanelTextures2
+            // 
+            resources.ApplyResources(flowLayoutPanelTextures2, "flowLayoutPanelTextures2");
+            flowLayoutPanelTextures2.BorderStyle = BorderStyle.FixedSingle;
+            flowLayoutPanelTextures2.Name = "flowLayoutPanelTextures2";
+            // 
+            // flowLayoutPanelAlphaImages
+            // 
+            resources.ApplyResources(flowLayoutPanelAlphaImages, "flowLayoutPanelAlphaImages");
+            flowLayoutPanelAlphaImages.BorderStyle = BorderStyle.FixedSingle;
+            flowLayoutPanelAlphaImages.Name = "flowLayoutPanelAlphaImages";
+            // 
+            // pictureBoxLandtile
+            // 
+            resources.ApplyResources(pictureBoxLandtile, "pictureBoxLandtile");
+            pictureBoxLandtile.BorderStyle = BorderStyle.FixedSingle;
+            pictureBoxLandtile.Name = "pictureBoxLandtile";
+            pictureBoxLandtile.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(pictureBoxLandtile);
+            Controls.Add(flowLayoutPanelAlphaImages);
+            Controls.Add(flowLayoutPanelTextures2);
+            Controls.Add(flowLayoutPanelTextures1);
+            Controls.Add(pictureBoxPreview);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(trackBarContrast);
+            Controls.Add(pictureBoxAlpha1);
+            Controls.Add(btnSelectAlpha);
+            Controls.Add(btnGenerateTransition);
+            Controls.Add(pictureBoxTexture2);
+            Controls.Add(btnSelectTexture2);
+            Controls.Add(btnSelectTexture1);
+            Controls.Add(pictureBoxTexture1);
+            Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBoxTexture1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxTexture2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxAlpha1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarContrast).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxPreview).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxLandtile).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxTexture1;
+        private System.Windows.Forms.Button btnSelectTexture1;
+        private Button btnSelectTexture2;
+        private PictureBox pictureBoxTexture2;
+        private System.Windows.Forms.Button btnSelectAlpha;
+        private System.Windows.Forms.Button btnGenerateTransition;
+        private PictureBox pictureBoxAlpha1;
+        private TrackBar trackBarContrast;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBoxPreview;
+        private FlowLayoutPanel flowLayoutPanelTextures1;
+        private FlowLayoutPanel flowLayoutPanelTextures2;
+        private FlowLayoutPanel flowLayoutPanelAlphaImages;
+        private TrackBar trackBarFlou;
+        private PictureBox pictureBoxLandtile;
+    }
+}
---
+++ Transitions/Form1.cs
@@ -0,0 +1,429 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Transitions
+{
+    public partial class Form1 : Form
+    {
+        private List<Image> textures1 = new List<Image>();
+        private List<Image> textures2 = new List<Image>();
+        private List<Image> alphaImages = new List<Image>();
+        private double gamma = 0.5; // Valeur par défaut pour gamma
+        private double blurValue = 0.5; // Valeur par défaut pour le flou
+        private FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+            trackBarContrast.Minimum = 1;
+            trackBarContrast.Maximum = 100;
+            trackBarContrast.Value = 1; // Valeur par défaut
+            trackBarContrast.TickFrequency = 10; // Fréquence des marqueurs sur la piste
+            //trackBarFlou.Minimum = 1;
+            //trackBarFlou.Maximum = 100;
+            //trackBarFlou.Value = 1; // Valeur par défaut
+            //trackBarFlou.TickFrequency = 10; // Fréquence des marqueurs sur la piste
+            pictureBoxTexture1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxTexture2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxAlpha1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxPreview.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxLandtile.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void UpdatePictureBoxes()
+        {
+            // Effacer d'abord tous les contrôles des FlowLayoutPanels
+            flowLayoutPanelTextures1.Controls.Clear();
+            flowLayoutPanelTextures2.Controls.Clear();
+            flowLayoutPanelAlphaImages.Controls.Clear();
+
+            // Afficher les textures 1
+            foreach (Image texture in textures1)
+            {
+                AddPictureBoxToFlowLayout(flowLayoutPanelTextures1, texture, 128, 128);
+            }
+
+            // Afficher les textures 2
+            foreach (Image texture in textures2)
+            {
+                AddPictureBoxToFlowLayout(flowLayoutPanelTextures2, texture, 128, 128);
+            }
+
+            // Afficher les alphaImages
+            foreach (Image alphaImage in alphaImages)
+            {
+                AddPictureBoxToFlowLayout(flowLayoutPanelAlphaImages, alphaImage, 128, 128);
+            }
+        }
+
+        private void AddPictureBoxToFlowLayout(FlowLayoutPanel flowLayoutPanel, Image image, int width, int height)
+        {
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Image = image;
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.Size = new Size(width, height);
+
+            flowLayoutPanel.Controls.Add(pictureBox);
+        }
+
+
+
+        private Image RotateAndResizeImageForPreview(Image image, double angle)
+        {
+            // Créer une nouvelle image avec la taille spécifiée
+            Bitmap rotatedImage = new Bitmap(46, 45);
+
+            // Créer une matrice de transformation pour la rotation
+            Matrix matrix = new Matrix();
+            matrix.Translate(23, 22); // Déplacer l'origine légèrement vers la droite pour compenser l'ajustement
+            matrix.Rotate((float)angle); // Rotation
+            matrix.Translate(-16, -16); // Ajustement après la rotation
+
+            // Dessiner l'image d'origine rotatée sur la nouvelle image
+            using (Graphics graphics = Graphics.FromImage(rotatedImage))
+            {
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.Transform = matrix;
+
+                // Dessiner l'image d'origine sur la nouvelle image en ajustant la taille pour qu'elle mesure exactement 46x45 pixels
+                graphics.DrawImage(image, -1, 0, 34, 33);
+            }
+
+            return rotatedImage;
+        }
+
+
+
+
+
+
+        private void UpdatePreview()
+        {
+            if (alphaImages.Count > 0 && textures1.Count > 0 && textures2.Count > 0)
+            {
+                Image texture1 = textures1[0];
+                Image texture2 = textures2[0];
+                Image alphaImage = alphaImages[0];
+
+                // Générer la première image de transition
+                Bitmap transitionImage = GenerateTransition(texture1, texture2, alphaImage);
+
+                // Afficher la première image de transition dans pictureBoxPreview
+                pictureBoxPreview.Image = transitionImage;
+
+                // Redimensionner et pivoter l'image pour la prévisualisation
+                Image previewImage = RotateAndResizeImageForPreview(transitionImage, 45);
+
+                // Afficher la prévisualisation dans pictureBoxLandtile
+                pictureBoxLandtile.Image = previewImage;
+            }
+        }
+
+
+        private void trackBarContrast_Scroll(object sender, EventArgs e)
+        {
+            gamma = (double)trackBarContrast.Value;
+            UpdatePreview();
+        }
+
+        private void trackBarFlou_Scroll(object sender, EventArgs e)
+        {
+            blurValue = (double)trackBarFlou.Value / 10.0 * 2;
+            UpdatePreview();
+        }
+
+        private Bitmap GenerateTransition(Image texture1, Image texture2, Image alphaImage)
+        {
+            Bitmap blurredAlphaImage = ApplyGamma(ApplyGaussianBlur((Bitmap)alphaImage, blurValue), gamma);
+            Bitmap transitionImage = new Bitmap(texture1.Width, texture1.Height);
+
+            for (int x = 0; x < texture1.Width; x++)
+            {
+                for (int y = 0; y < texture1.Height; y++)
+                {
+                    Color alphaPixel = blurredAlphaImage.GetPixel(x, y);
+                    float alpha = alphaPixel.GetBrightness();
+
+                    Color texture1Pixel = ((Bitmap)texture1).GetPixel(x, y);
+                    Color texture2Pixel = ((Bitmap)texture2).GetPixel(x, y);
+
+                    int newRed = (int)(alpha * texture1Pixel.R + (1 - alpha) * texture2Pixel.R);
+                    int newGreen = (int)(alpha * texture1Pixel.G + (1 - alpha) * texture2Pixel.G);
+                    int newBlue = (int)(alpha * texture1Pixel.B + (1 - alpha) * texture2Pixel.B);
+
+                    newRed = Math.Max(0, Math.Min(255, newRed));
+                    newGreen = Math.Max(0, Math.Min(255, newGreen));
+                    newBlue = Math.Max(0, Math.Min(255, newBlue));
+
+                    transitionImage.SetPixel(x, y, Color.FromArgb(newRed, newGreen, newBlue));
+                }
+            }
+
+            return transitionImage;
+        }
+
+        private Bitmap ApplyGaussianBlur(Bitmap image, double blurValue)
+        {
+            BlurFilter filter = new BlurFilter();
+            filter.Sigma = blurValue;
+            return filter.ProcessImage(image);
+        }
+
+        private Bitmap ApplyGamma(Bitmap image, double gamma)
+        {
+            Bitmap adjustedImage = new Bitmap(image.Width, image.Height);
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color originalPixel = image.GetPixel(x, y);
+                    int newRed = (int)(255 * Math.Pow(originalPixel.R / 255.0, gamma));
+                    int newGreen = (int)(255 * Math.Pow(originalPixel.G / 255.0, gamma));
+                    int newBlue = (int)(255 * Math.Pow(originalPixel.B / 255.0, gamma));
+
+                    newRed = Math.Max(0, Math.Min(255, newRed));
+                    newGreen = Math.Max(0, Math.Min(255, newGreen));
+                    newBlue = Math.Max(0, Math.Min(255, newBlue));
+
+                    adjustedImage.SetPixel(x, y, Color.FromArgb(newRed, newGreen, newBlue));
+                }
+            }
+
+            return adjustedImage;
+        }
+
+        private void btnSelectTexture1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.png;*.jpg;*.jpeg;*.gif;*.bmp)|*.png;*.jpg;*.jpeg;*.gif;*.bmp";
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textures1.Clear();
+                foreach (string imagePath in openFileDialog.FileNames)
+                {
+                    try
+                    {
+                        Image texture1 = Image.FromFile(imagePath);
+                        textures1.Add(texture1);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Une erreur s'est produite lors du chargement de l'image : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                UpdatePictureBoxes();
+                UpdatePreview();
+            }
+        }
+
+        private void btnSelectTexture2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.png;*.jpg;*.jpeg;*.gif;*.bmp)|*.png;*.jpg;*.jpeg;*.gif;*.bmp";
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textures2.Clear();
+                foreach (string imagePath in openFileDialog.FileNames)
+                {
+                    try
+                    {
+                        Image texture2 = Image.FromFile(imagePath);
+                        textures2.Add(texture2);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Une erreur s'est produite lors du chargement de l'image : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                UpdatePictureBoxes();
+                UpdatePreview();
+            }
+        }
+
+
+        private void btnSelectAlpha_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.Description = "Sélectionner le dossier contenant les images alpha";
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = folderBrowserDialog.SelectedPath;
+
+                try
+                {
+                    alphaImages.Clear();
+                    string[] alphaFiles = Directory.GetFiles(folderPath, "*.png");
+
+                    foreach (string filePath in alphaFiles)
+                    {
+                        Image alphaImage = Image.FromFile(filePath);
+                        alphaImages.Add(alphaImage);
+                    }
+
+                    MessageBox.Show($"Chargement réussi : {alphaImages.Count} images alpha chargées.", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Une erreur s'est produite lors du chargement des images alpha : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                UpdatePictureBoxes();
+                UpdatePreview();
+            }
+        }
+
+
+
+
+
+
+
+        private void btnGenerateTransition_Click(object sender, EventArgs e)
+        {
+            if (textures1.Count == 0 || textures2.Count == 0 || alphaImages.Count == 0)
+            {
+                MessageBox.Show("Veuillez sélectionner les textures et les images alpha avant de générer la transition.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                int alphaIndex = 0;
+
+                foreach (Image alphaImage in alphaImages)
+                {
+                    Image texture1 = textures1[alphaIndex % textures1.Count];
+                    Image texture2 = textures2[alphaIndex % textures2.Count];
+                    Bitmap transitionImage = GenerateTransition(texture1, texture2, alphaImage);
+
+                    // Enregistrer les images sans rotation
+                    string transitionFileName = Path.Combine(folderBrowserDialog.SelectedPath, $"Transition_{alphaIndex + 1}.bmp");
+                    transitionImage.Save(transitionFileName, ImageFormat.Bmp);
+
+                    // Prévisualiser la transition
+                    Image previewImage = RotateAndResizeImageForPreview(transitionImage, 45);
+
+                    // Enregistrer les images avec rotation à 45 degrés et redimensionnées
+                    string rotatedTransitionFileName = Path.Combine(folderBrowserDialog.SelectedPath, $"RotatedTransition_{alphaIndex + 1}.bmp");
+                    previewImage.Save(rotatedTransitionFileName, ImageFormat.Bmp);
+
+                    alphaIndex++;
+                }
+
+                MessageBox.Show("Génération terminée.", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Vider les listes
+                textures1.Clear();
+                textures2.Clear();
+                alphaImages.Clear();
+
+                // Effacer les PictureBox
+                pictureBoxTexture1.Image = null;
+                pictureBoxTexture2.Image = null;
+                pictureBoxAlpha1.Image = null;
+                pictureBoxPreview.Image = null;
+                pictureBoxLandtile.Image = null;
+
+                // Effacer les contrôles des FlowLayoutPanels
+                flowLayoutPanelTextures1.Controls.Clear();
+                flowLayoutPanelTextures2.Controls.Clear();
+                flowLayoutPanelAlphaImages.Controls.Clear();
+            }
+        }
+
+
+
+
+
+        // Définition de la classe BlurFilter pour le flou gaussien
+        public class BlurFilter
+        {
+            private double[] kernel;
+            private int kernelSize;
+
+            public double Sigma { get; set; }
+
+            public BlurFilter()
+            {
+                Sigma = 1.0;
+                kernelSize = 1;
+                kernel = GenerateGaussianKernel(Sigma, kernelSize);
+            }
+
+            public Bitmap ProcessImage(Bitmap image)
+            {
+                Bitmap result = new Bitmap(image.Width, image.Height);
+
+                int k = kernelSize / 2;
+                int r, g, b;
+
+                for (int x = k; x < image.Width - k; x++)
+                {
+                    for (int y = k; y < image.Height - k; y++)
+                    {
+                        r = g = b = 0;
+
+                        for (int i = -k; i <= k; i++)
+                        {
+                            for (int j = -k; j <= k; j++)
+                            {
+                                Color c = image.GetPixel(x + i, y + j);
+                                double w = kernel[i + k] * kernel[j + k];
+                                r += (int)(c.R * w);
+                                g += (int)(c.G * w);
+                                b += (int)(c.B * w);
+                            }
+                        }
+
+                        r = Math.Min(255, Math.Max(0, r));
+                        g = Math.Min(255, Math.Max(0, g));
+                        b = Math.Min(255, Math.Max(0, b));
+
+                        result.SetPixel(x, y, Color.FromArgb(r, g, b));
+                    }
+                }
+
+                return result;
+            }
+
+            private double Gaussian(double x, double sigma)
+            {
+                return Math.Exp(-(x * x) / (2 * sigma * sigma)) / (Math.Sqrt(2 * Math.PI) * sigma);
+            }
+
+            private double[] GenerateGaussianKernel(double sigma, int size)
+            {
+                double[] kernel = new double[size];
+                int center = size / 2;
+
+                for (int i = 0; i < size; i++)
+                {
+                    kernel[i] = Gaussian(i - center, sigma);
+                }
+
+                // Normalize the kernel
+                double sum = 0;
+                foreach (double value in kernel)
+                {
+                    sum += value;
+                }
+
+                for (int i = 0; i < size; i++)
+                {
+                    kernel[i] /= sum;
+                }
+
+                return kernel;
+            }
+        }
+    }
+}
